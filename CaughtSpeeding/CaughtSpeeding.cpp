256cc72e81f386b56e0554d0496ba7dbba184af8
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-05-11 13:58:32 -0700

    Created scenario setup and user prompt.

--- CaughtSpeeding/CaughtSpeeding.cpp
+++ CaughtSpeeding/CaughtSpeeding.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main()
 {
-
+	cout << "You are driving a little too fast, and a police officer stops you.\n";
+	cout << "How fast were you going? \n";
+	cout << "Your speed in mph: ";
 }
