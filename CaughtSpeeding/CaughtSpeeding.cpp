c41fc696f061ea800ee7f1de667e3d7506f8eec6
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-05-11 14:04:01 -0700

    Initial if statement checking cin userSpeed

--- CaughtSpeeding/CaughtSpeeding.cpp
+++ CaughtSpeeding/CaughtSpeeding.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
+	int userSpeed = 60;
 	cout << "You are driving a little too fast, and a police officer stops you.\n";
 	cout << "How fast were you going? \n";
 	cout << "Your speed in mph: ";
+	cin >> userSpeed;
+	cout << endl;
+	
+	if (userSpeed <= 60)
+	{
+		cout << "You are free to go. No speeding ticket this time...";
+	}
+
 }
