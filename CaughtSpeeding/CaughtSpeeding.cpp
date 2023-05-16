2e1d2cec994a40cc743beb36cb4f882d1172c9b4
Taylor Bisset <taylorbisset@gmail.com>
2023-05-16 11:39:10 -0700

    Added loop

--- CaughtSpeeding/CaughtSpeeding.cpp
+++ CaughtSpeeding/CaughtSpeeding.cpp
@@ -6,27 +6,30 @@ int main()
 {
 	int userSpeed = 60;
 
-	cout << "You are driving a little too fast, and a police officer stops you.\n";
-	cout << "How fast were you going? \n";
-	cout << "Your speed in mph: ";
-	
-	cin >> userSpeed;
-	cout << endl;
-	
-	if (userSpeed <= 60)
+	while (true)
 	{
-		cout << "You are free to go. No speeding ticket this time...\n";
-	}
-	else if (userSpeed > 60 && userSpeed <= 80)
-	{
-		cout << "You're getting a small ticket. Please slow down.\n";
-	}
-	else if (userSpeed > 80 && userSpeed <= 100)
-	{
-		cout << "You are getting a big ticket for your speeding. You better watch your speed!\n";
-	}
-	else if (userSpeed > 100)
-	{
-		cout << "Alright buddy. Your car is being impounded... And you're off to the slammer!\n";
+		cout << "You are driving a little too fast, and a police officer stops you.\n";
+		cout << "How fast were you going? \n";
+		cout << "Your speed in mph: ";
+
+		cin >> userSpeed;
+		cout << endl;
+
+		if (userSpeed <= 60)
+		{
+			cout << "You are free to go. No speeding ticket this time...\n";
+		}
+		else if (userSpeed > 60 && userSpeed <= 80)
+		{
+			cout << "You're getting a small ticket. Please slow down.\n";
+		}
+		else if (userSpeed > 80 && userSpeed <= 100)
+		{
+			cout << "You are getting a big ticket for your speeding. You better watch your speed!\n";
+		}
+		else if (userSpeed > 100)
+		{
+			cout << "Alright buddy. Your car is being impounded... And you're off to the slammer!\n";
+		}
 	}
 }
